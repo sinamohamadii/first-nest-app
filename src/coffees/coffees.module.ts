3271b53c1a5f88d0b0a351d279e4231e3933d1d0
sinamohamadii <sina.m925@gmail.com>
2026-06-24 21:38:08 +0330

    Add docker compose file, TypeORM package and add coffees entity to the database

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffees } from './entities/coffees.entity';
 
 @Module({
+  // Import the Coffees entity into the ORM module is required to register the entity
+  imports: [TypeOrmModule.forFeature([Coffees])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
 })
