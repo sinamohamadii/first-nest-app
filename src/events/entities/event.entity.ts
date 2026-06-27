9bdfa4256de6de7b9505665578ef8030e221f378
sinamohamadii <sina.m925@gmail.com>
2026-06-27 14:44:47 +0330

    Add Inedx for event table using TypeORM

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,16 +1,21 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, Index, PrimaryGeneratedColumn } from 'typeorm';
 
+// Addin index here like this can get multiple indexes with different columns:
+// @Index(["name, "type"])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
   id!: number;
 
+  // Adding index
+  @Index()
   @Column()
   name!: string;
 
   @Column()
   type!: string;
 
+  // an object that keys are strings and the values can be anything
   @Column('json')
   payload!: Record<string, any>;
 }
