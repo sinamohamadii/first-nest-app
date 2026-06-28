47b5dee1906db9fe0d09aad48d1da8261d51f08c
sinamohamadii <sina.m925@gmail.com>
2026-06-28 12:51:00 +0330

    Add nest.js config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,22 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      // envFilePath: '.environment',
+      // for times that we dont want to use env file ignoreEnvFile: true,
+    }),
     CoffeesModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: +(process.env.DATABASE_PORT || 5432),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       autoLoadEntities: true,
       synchronize: true,
     }),
