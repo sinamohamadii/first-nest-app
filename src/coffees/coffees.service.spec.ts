0f1494e6887bc8b6b660f84795db900ca2aa4dd6
sinamohamadii <sina.m925@gmail.com>
2026-07-01 16:38:03 +0330

    Create jest branch and wrote a simple test

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Flavor } from './entities/flavor.entity/flavor.entity';
+import { Coffees } from './entities/coffees.entity';
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: DataSource, useValue: { createQueryRunner: jest.fn() } },
+        { provide: ConfigService, useValue: { get: jest.fn().mockReturnValue('localhost') } },
+        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffees), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
